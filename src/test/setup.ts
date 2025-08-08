20f3a2fe4749576f98f87584d58fa942a9ca5230
James Tatum <jtatum@gmail.com>
2025-08-07 17:58:24 -0700

    tests(ui): stub scrollIntoView in jsdom and await selection updates

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,2 +1,7 @@
 import '@testing-library/jest-dom/vitest';
 
+// jsdom does not implement scrollIntoView; stub it to a no-op for tests
+if (!Element.prototype.scrollIntoView) {
+  // @ts-expect-error jsdom environment
+  Element.prototype.scrollIntoView = () => {};
+}
